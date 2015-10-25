e4f6fc0c1c73f5f970823bb5f9f550feea6a0f72
Eugene Mirotin <emirotin@gmail.com>
2015-10-25 04:52:56 +0300

    log

--- app/start.sh
+++ app/start.sh
@@ -5,6 +5,7 @@ udevadm trigger
 if [ ! -c /dev/fb1 ]; then
   echo "loading piTFT kernel module"
   modprobe spi-bcm2708
+  modprobe fbtft_device name=list; dmesg | tail -30
   modprobe fbtft_device name=adafruitrt35 verbose=1 rotate=270
 
   sleep 1
