31a02079ad3b62ab323b442c8388f99f7039918c
Eugene Mirotin <emirotin@gmail.com>
2015-10-28 12:34:06 +0300

    1

--- app/server.js
+++ app/server.js
@@ -7,20 +7,30 @@ console.log("starting node.js script...")
 // Clear the back buffer
 fb.clear();
 
-console.log('size', fb.size());
+
+var LINE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var INTERVAL = 1600;
+
+var index = _.parseInt(process.env.INDEX || '1') - 1;
+var count = _.parseInt(process.env.COUNT || '1') - 1;
+var lineLength = LINE.length;
+
 var xMax = fb.size().width;
 var yMax = fb.size().height;
 
 var draw = function() {
+  var time = Date.now();
+  var shift = (~~(time / INTERVAL) + index) % lineLength;
+
   fb.clear();
   fb.color(1, 1, 1);
   fb.font("fantasy", 320, true);
-  var letter = _.sample('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-  fb.text(xMax / 2 - 30, yMax / 2, letter, true, 0);
+  var letter = _.sample();
+  fb.text(xMax / 2 - 30, yMax / 2, LINE[shift], true, 0);
   fb.blit();
 };
 
 draw();
 setInterval(function() {
   draw();
-}, 500);
+}, INTERVAL / 2);
